32cbd12b0bd864af62260f8bfb21990e41b871e8
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2022-11-15 00:10:25 +0900

    refactor: move installing packages to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 mv "${HOME}/.zshrc" "${HOME}/.zshrc.codespaces"
 ln -f -s "${PWD}/.settings" "${HOME}/.settings"
 ln -f -s "${PWD}/.settings/zsh/zshrc" "${HOME}/.zshrc"
+
+if [[ -s "${HOME}/.settings/commands/package-install.zsh" ]]; then
+    /bin/zsh "${HOME}/.settings/commands/package-install.zsh"
+fi
