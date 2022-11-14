32659878b67b77c0f505008c8462d8af66fbe0e4
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2022-11-15 00:31:34 +0900

    fix: error of ls color flag

--- .settings/zsh/aliases.zsh
+++ .settings/zsh/aliases.zsh
@@ -1,11 +1,11 @@
 # List all files colorized in long format
-alias ll="ls -lFh ${colorflag}"
+alias ll="ls -lFh --color"
 
 # List all files colorized in long format, excluding . and ..
-alias la="ls -lAFh ${colorflag}"
+alias la="ls -lAFh --color"
 
 # List only directories
-alias lsd="ls -lFh ${colorflag} | grep --color=never '^d'"
+alias lsd="ls -lFh --color | grep --color=never '^d'"
 
 # Always use color output for `ls`
-alias ls="command ls ${colorflag}"
+alias ls="command ls --color"
