55ee27028e28d2bc2c6dcdc0af54673fe51bbe93
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2022-11-15 11:30:31 +0900

    feat: add gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mv "${HOME}/.zshrc" "${HOME}/.zshrc.default"
 ln -f -s "${PWD}/.settings" "${HOME}/.settings"
 ln -f -s "${HOME}/.settings/zsh/zshrc" "${HOME}/.zshrc"
+ln -f -s "${HOME}/.settings/git/config" "${HOME}/.gitconfig"
 
 /bin/zsh "${HOME}/.settings/commands/package-install.zsh"
