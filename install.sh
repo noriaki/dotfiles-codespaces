5df6f2a0c2587a6204179ee89911ca8d9ca77b90
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2022-11-15 12:15:19 +0900

    feat: add tig

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ mv "${HOME}/.zshrc" "${HOME}/.zshrc.default"
 ln -f -s "${PWD}/.settings" "${HOME}/.settings"
 ln -f -s "${HOME}/.settings/zsh/zshrc" "${HOME}/.zshrc"
 ln -f -s "${HOME}/.settings/git/config" "${HOME}/.gitconfig"
+ln -f -s "${HOME}/.settings/tig/config" "${HOME}/.tigrc"
 
 /bin/zsh "${HOME}/.settings/commands/package-install.zsh"
